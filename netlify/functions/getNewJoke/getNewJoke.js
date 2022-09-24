1f4b915e91dfc083e52e300e085020f605054bc5
WanerValencia <waner.valencia@antpack.co>
2022-09-24 09:27:45 -0500

    get new joke

--- netlify/functions/getNewJoke/getNewJoke.js
+++ netlify/functions/getNewJoke/getNewJoke.js
@@ -7,26 +7,46 @@ const supabase = createClient(
 	"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRqbm1odHVtaHlhd3ppZmxxenNvIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjM5NzI5NjQsImV4cCI6MTk3OTU0ODk2NH0.uEnkQpKqTZUY6zmDb7o3NDjk08dgQZAYy7M47siDU2Y"
 );
 
-const handler = async function (event, context) {
-	if (event.httpMethod !== "GET")
-		return { statusCode: 405, body: "Must GET to this function" };
+const CORS_HEADERS = {
+	"Access-Control-Allow-Methods": "GET , DELETE, POST, OPTIONS",
+	"Access-Control-Allow-Origin": "*",
+	"Access-Control-Allow-Headers":
+		"Origin, Access-Control-Allow-Origin, X-Requested-With, Content-Type, Accept, Authorization, authorization",
+	"Access-Control-Max-Age": "2592000",
+	"Access-Control-Allow-Credentials": "true",
+	"Content-Type": "application/json",
+	"Vary": "Origin"
+};
 
-	const token = event.headers["authorization"]
+const handler = async function (event, context) {
+
+	if (event.httpMethod !== "GET")
+		return {
+			statusCode: 405,
+			body: "Must GET to this function",
+			headers: CORS_HEADERS,
+		};
+
+	const token = event.queryStringParameters["token"];
 
 	if (!token) {
 		return {
 			statusCode: 401,
 			body: JSON.stringify({ message: "Token is required" }),
+			headers: CORS_HEADERS,
 		};
 	}
 
 	try {
-		const { error, user } = await supabase.auth.api.getUser(token)
+		const { error } = await supabase.auth.api.getUser(token);
 
-		if(error) {
+		if (error) {
 			return {
+				headers: CORS_HEADERS,
 				statusCode: error.status,
-				body: JSON.stringify({ message: "Invalid token, signOut and retry later"}),
+				body: JSON.stringify({
+					message: "Invalid token, signOut and retry later",
+				}),
 			};
 		}
 
@@ -39,7 +59,7 @@ const handler = async function (event, context) {
 				statusCode: response.status,
 				body: JSON.stringify({ message: "Sorry, We can't get your joke" }),
 				headers: {
-					"Content-Type": "application/json",
+					...CORS_HEADERS,
 				},
 			};
 		}
@@ -48,11 +68,13 @@ const handler = async function (event, context) {
 
 		return {
 			statusCode: 200,
+			headers: CORS_HEADERS,
 			body: JSON.stringify(data),
 		};
 	} catch (error) {
 		return {
 			statusCode: 500,
+			headers: CORS_HEADERS,
 			body: JSON.stringify({ message: error.message }),
 		};
 	}
